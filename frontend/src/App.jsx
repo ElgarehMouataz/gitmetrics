67f4fc0ea0c5f7e7d79de75a2816ecca0f3931a7
MouatazElgareh <mouatazelgareh044@gmail.com>
2026-05-16 22:08:47 +0100

    file structure and dependancies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>GitMetrics Development Environment</h1>
+      <p>Edit src/App.jsx to start developing.</p>
+    </div>
+  )
+}
+
+export default App
